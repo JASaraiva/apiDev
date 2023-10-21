12011732a4d50497a5dce0ea8de35f78b0c21cc6
Jonas A. Saraiva <jonas.a.saraiva@accenture.com>
2023-10-21 16:17:35 -0300

    implementando primeiras rotas

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from "@nestjs/common";
 
 @Controller('users')
 
@@ -8,5 +8,46 @@ export class UserContoller{
     async create(@Body() body){
 
         return {body};
+    }
+
+    @Get()
+    async list(){
+
+        return {users: []};
+    }
+
+    @Get(':id')
+    async readOne(@Param() param){
+
+        return {user: [], param};
+    }
+
+    @Put(':id')
+    async update(@Param() param, @Body() body){
+
+        return {
+                method: 'PUT',
+                body,
+                param
+            };
+            
+    }
+
+    @Patch(':id')
+    async updatePartial(@Param() param, @Body() body){
+
+        return {
+                method: 'PUT',
+                body,
+                param
+            };
+    }
+
+    @Delete(':id')
+    async delete(@Param() param){
+
+        return {
+                param
+            };
     }
 }
