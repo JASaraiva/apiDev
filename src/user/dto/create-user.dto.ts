95bc38faa13c9f44efd1a345e0b42a55c1004fe3
Jonas A. Saraiva <jonas.a.saraiva@accenture.com>
2023-10-21 18:13:59 -0300

    implementando banco de dados

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,20 @@
+import { IsString, IsEmail, IsStrongPassword } from "class-validator";
+
+export class CreateUserDTO{
+
+    @IsString()
+    name: string;
+
+    @IsEmail()
+    email: string;
+
+    @IsStrongPassword({
+        minLength: 8,
+        minLowercase: 0,
+        minUppercase: 1,
+        minNumbers: 0,
+        minSymbols: 1
+    })
+    password: string;
+
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,53 +1,57 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Put } from "@nestjs/common";
+import { CreateUserDTO } from "./dto/create-user.dto";
+import { UpdatePutUserDTO } from "./dto/update-put-user.dto";
+import { UpdatePatchUserDTO } from "./dto/update-patch-user.dto";
 
 @Controller('users')
 
 export class UserContoller{
 
     @Post()
-    async create(@Body() body){
-
-        return {body};
+    async create(@Body() {email,name,password}: CreateUserDTO){
+        return {email,name,password};
     }
 
     @Get()
     async list(){
-
         return {users: []};
     }
 
     @Get(':id')
-    async readOne(@Param() param){
-
-        return {user: [], param};
+    async readOne(@Param('id', ParseIntPipe) id: number){
+        return {
+                user: [], 
+                id
+            };
     }
 
     @Put(':id')
-    async update(@Param() param, @Body() body){
-
+    async update(@Param('id', ParseIntPipe) id: number, @Body() { name, email, password } : UpdatePutUserDTO){
         return {
                 method: 'PUT',
-                body,
-                param
+                name, 
+                email, 
+                password,
+                id
             };
             
     }
 
     @Patch(':id')
-    async updatePartial(@Param() param, @Body() body){
-
+    async updatePartial(@Param('id', ParseIntPipe) id: number, @Body() { name, email, password } : UpdatePatchUserDTO){
         return {
                 method: 'PUT',
-                body,
-                param
+                name, 
+                email, 
+                password,
+                id
             };
     }
 
     @Delete(':id')
-    async delete(@Param() param){
-
+    async delete(@Param('id', ParseIntPipe) id: number){
         return {
-                param
+                id
             };
     }
 }
