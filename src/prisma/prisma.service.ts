b45bdf30b43f4f2eebdfd1208ec991cafa538a6b
Jonas A. Saraiva <jonas.a.saraiva@accenture.com>
2023-10-21 21:22:15 -0300

    criando services de banco

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,10 +1,11 @@
-import { Injectable, OnModuleInit } from "@nestjs/common";
+import { INestApplication, Injectable, OnModuleInit } from "@nestjs/common";
 import { PrismaClient } from "@prisma/client";
+
 
 @Injectable()
 
 export class PrismaService extends PrismaClient implements OnModuleInit{
-    onModuleInit(){
-        this
+    async onModuleInit(){
+        await this.$connect()
     }
 }
